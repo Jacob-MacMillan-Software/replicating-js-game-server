3343a853344c5136aec1e3858d93615e9030a4d1
Jacob <31115470+MrMcGoats@users.noreply.github.com>
2023-04-05 02:04:20 -0700

    fixed bug where client would no longer show other players after a battle

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import WebSocket, { WebSocketServer } from 'ws';
 const wss = new WebSocketServer({port: 3000});
 
 const clients = {};
+const lastLocation = {};
 
 function updateAllClients(message: string) {
   console.log(`Broadcasting message: ${message}`);
@@ -18,50 +19,77 @@ function updateAllClients(message: string) {
 wss.on('connection', (ws: WebSocket) => {
   // generate client ID
   const clientId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-  const lastLocation = {x: 0, y: 0};
-  
+  // @ts-ignore
+  lastLocation[clientId] = {x: 0, y: 0};
+
   // add client to clients object
   // idk how to define the type
   // @ts-ignore
   clients[clientId] = ws;
 
-  ws.on('message', (message: string[]) => {
-    console.log(`Received message from ${clientId} => ${message}`);
-    const messagearr = message.toString().split(',');
-    const parsedMessage = [parseFloat(messagearr[0]), parseFloat(messagearr[1])];
-    console.log(parsedMessage);
-    
-    
-    // TODO: check if the player can actually walk there
-    // for now just assume they can
-    lastLocation.x = parsedMessage[0];
-    lastLocation.y = parsedMessage[1];
+  ws.on('message', (rawMessage: string) => {
+    console.log(`Received message from ${clientId} => ${rawMessage}`);
 
-    updateAllClients(`${clientId}:position:${parsedMessage[0]},${parsedMessage[1]}`);
+    // parse message
+    const [req, message] = rawMessage.toString().split(':');
 
-    console.log(lastLocation);
+    if(req === 'position') {
+      const messagearr = message.split(',');
+      const parsedMessage = [parseFloat(messagearr[0]), parseFloat(messagearr[1])];
+      console.log(parsedMessage);
+
+
+      // TODO: check if the player can actually walk there
+      // for now just assume they can
+      // @ts-ignore
+      lastLocation[clientId].x = parsedMessage[0];
+      // @ts-ignore
+      lastLocation[clientId].y = parsedMessage[1];
+
+      updateAllClients(`${clientId}:position:${parsedMessage[0]},${parsedMessage[1]}`);
+
+      console.log(lastLocation);
+    } else if(req === "getPlayer") {
+      if(message === 'all') {
+        // @ts-ignore
+        for (const otherClientId in clients) {
+          if(otherClientId !== clientId) {
+            // @ts-ignore
+            ws.send(`connected:${otherClientId}`);
+            // @ts-ignore
+            ws.send(`${otherClientId}:position:${lastLocation[otherClientId].x},${lastLocation[otherClientId].y}`);
+          }
+        }
+      } else {
+        // @ts-ignore
+        ws.send(`connected:${message}`);
+        // @ts-ignore
+        ws.send(`${clientId}:position:${lastLocation[message].x},${lastLocation[message].y}`);
+      }
+    }
+
   });
 
   ws.on('error', (error: Error) => {
     console.error(error);
     // @ts-ignore
     delete clients[clientId];
-    
+
     // Notify all clients of disconnection
     updateAllClients(`disconnected:${clientId}`);
   });
-  
+
   ws.on('close', () => {
     console.log(`Client ${clientId} disconnected`);
     // @ts-ignore
     delete clients[clientId];
-    
+
     // Notify all clients of disconnection
     updateAllClients(`disconnected:${clientId}`);
   });
 
   ws.send(`id:${clientId}`);
-  
+
   // Notify client of all other clients
   // @ts-ignore
   for (const otherClientId in clients) {
@@ -69,7 +97,7 @@ wss.on('connection', (ws: WebSocket) => {
       ws.send(`connected:${otherClientId}`);
     }
   }
-  
+
   // Inform all connected clients of new player
   updateAllClients(`connected:${clientId}`);
 
